7a798533a7bbaed6bfcd11803b957f38ca2f2f52
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-10-24 20:17:42 -0300

    Add files via upload

---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret;
+            ret = new Retangulo();
+
+            Console.Write("Informe o valor da base do retângulo: ");
+            ret.setBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o valor da altura do retângulo: ");
+            ret.setAltura(int.Parse(Console.ReadLine()));
+
+            ret.calcularArea();
+
+            Console.WriteLine("Área do retângulo: {0}", ret.getArea());
+        }
+    }
+}
