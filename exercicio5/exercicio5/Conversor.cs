4549215698750c258d1955d8326d0e8ccc7c7346
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:05:59 -0300

    Add files via upload

---
+++ exercicio5/exercicio5/Conversor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5
+{
+    public class Conversor
+    {
+        public double ConverterMilhasParaQuilometros(double milhasMaritimas)
+        {
+            double metros = milhasMaritimas * 1852;
+            double quilometros = metros / 1000;
+            return quilometros;
+        }
+    }
+}
---
+++ exercicio5/exercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor em milhas marítimas: ");
+            double milhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            Conversor conversor = new Conversor();
+
+            double quilometros = conversor.ConverterMilhasParaQuilometros(milhasMaritimas);
+            Console.WriteLine($"{milhasMaritimas} milhas marítimas equivalem a {quilometros} quilômetros.");
+        }
+    }
+}
