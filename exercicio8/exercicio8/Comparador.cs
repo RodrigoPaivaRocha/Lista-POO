4e9b3d42069994019b971d3bda5169b508d52084
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:07:29 -0300

    Add files via upload

---
+++ exercicio8/exercicio8/Comparador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio8
+{
+    public class Comparador
+    {
+        public string ObterMaior(double valor1, double valor2)
+        {
+            if (valor1 > valor2)
+            {
+                return $"O maior valor é: {valor1}";
+            }
+            else if (valor2 > valor1)
+            {
+                return $"O maior valor é: {valor2}";
+            }
+            else
+            {
+                return "Os números são idênticos.";
+            }
+        }
+    }
+}
---
+++ exercicio8/exercicio8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Comparador comparador = new Comparador();
+
+            string resultado = comparador.ObterMaior(valor1, valor2);
+            Console.WriteLine(resultado);
+        }
+    }
+}
