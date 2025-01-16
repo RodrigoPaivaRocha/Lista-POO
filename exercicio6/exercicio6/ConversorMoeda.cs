a9cdf41ad2753a755f9f26e5a98b91e6886e8a2e
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:06:33 -0300

    Add files via upload

---
+++ exercicio6/exercicio6/ConversorMoeda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    public class ConversorMoeda
+    {
+        public double CotacaoDolar { get; set; }
+
+        public double QuantidadeDolares { get; set; }
+
+        public ConversorMoeda(double cotacaoDolar, double quantidadeDolares)
+        {
+            CotacaoDolar = cotacaoDolar;
+            QuantidadeDolares = quantidadeDolares;
+        }
+
+        public double ConverterParaReais()
+        {
+            return CotacaoDolar * QuantidadeDolares;
+        }
+    }
+}
---
+++ exercicio6/exercicio6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da cotação do dólar: ");
+            double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de dólares: ");
+            double quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+
+            ConversorMoeda conversor = new ConversorMoeda(cotacaoDolar, quantidadeDolares);
+
+            double valorEmReais = conversor.ConverterParaReais();
+            Console.WriteLine($"O valor correspondente em reais é: R$ {valorEmReais:F2}");
+        }
+    }
+}
