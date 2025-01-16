2bc5c9431dd022b6651527b52c077fa6ec7f5e42
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:05:32 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da base do triângulo: ");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura do triângulo: ");
+            double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(baseTriangulo, alturaTriangulo);
+
+            double area = triangulo.CalcularArea();
+            Console.WriteLine($"A área do triângulo é: {area}");
+        }
+    }
+}
---
+++ exercicio4/exercicio4/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    public class Triangulo
+    {
+        public double Base { get; set; }
+
+        public double Altura { get; set; }
+
+        public Triangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            Base = baseTriangulo;
+            Altura = alturaTriangulo;
+        }
+
+        public double CalcularArea()
+        {
+            return (Base * Altura) / 2;
+        }
+    }
+}
