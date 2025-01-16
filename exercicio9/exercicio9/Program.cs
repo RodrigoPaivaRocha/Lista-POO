ac600d2e1119ee34a0570247c03682c73a1edf59
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:07:58 -0300

    Add files via upload

---
+++ exercicio9/exercicio9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da base do retângulo: ");
+            double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura do retângulo: ");
+            double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(baseRetangulo, alturaRetangulo);
+
+            string resultado = retangulo.AvaliarTerreno();
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ exercicio9/exercicio9/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio9
+{
+    internal class Retangulo
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo(double baseRetangulo, double alturaRetangulo)
+        {
+            Base = baseRetangulo;
+            Altura = alturaRetangulo;
+        }
+
+        public string AvaliarTerreno()
+        {
+            double area = Base * Altura;
+            if (area > 100)
+            {
+                return $"A área do terreno é {area}. Terreno grande.";
+            }
+            else
+            {
+                return $"A área do terreno é {area}. Terreno pequeno.";
+            }
+        }
+    }
+}
