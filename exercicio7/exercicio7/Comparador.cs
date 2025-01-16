fc6619ea5bb0eab72aa206d930a6250546cfd121
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:07:02 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Comparador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    internal class Comparador
+    {
+        public double ObterMaior(double valor1, double valor2)
+        {
+            return valor1 > valor2 ? valor1 : valor2;
+        }
+    }
+}
---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Comparador comparador = new Comparador();
+
+            double maiorValor = comparador.ObterMaior(valor1, valor2);
+            Console.WriteLine($"O maior valor é: {maiorValor}");
+        }
+    }
+}
