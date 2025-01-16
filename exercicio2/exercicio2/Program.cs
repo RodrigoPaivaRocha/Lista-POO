4390400bb523c57d70217c7aaa43b5a629bf9c0b
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:01:27 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da aresta do quadrado: ");
+            double aresta = Convert.ToDouble(Console.ReadLine());
+
+            Quadrado quadrado = new Quadrado(aresta);
+
+            double area = quadrado.CalcularArea();
+            Console.WriteLine($"A área do quadrado é: {area}");
+        }
+    }
+}
---
+++ exercicio2/exercicio2/Quadrado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    public class Quadrado
+    {
+        public double Aresta { get; set; }
+
+        public Quadrado(double aresta)
+        {
+            Aresta = aresta;
+        }
+        public double CalcularArea()
+        {
+            return Aresta * Aresta;
+        }
+    }
+}
