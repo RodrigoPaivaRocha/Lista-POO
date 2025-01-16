1a46c54b96be18408e054509d0e89fd17ca8c4b7
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:03:22 -0300

    Add files via upload

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            double diagonal = Convert.ToDouble(Console.ReadLine());
+
+            Quadrado quadrado = new Quadrado(diagonal);
+
+            double area = quadrado.CalcularArea();
+            Console.WriteLine($"A área do quadrado é: {area}");
+        }
+    }
+}
---
+++ exercicio3/exercicio3/Quadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    public class Quadrado
+    {
+        public double Diagonal { get; set; }
+
+        public Quadrado(double diagonal)
+        {
+            Diagonal = diagonal;
+        }
+
+        public double CalcularArea()
+        {
+            return (Diagonal * Diagonal) / 2;
+        }
+    }
+}
