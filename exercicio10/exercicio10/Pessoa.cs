a17bb65ae460bb725ff184ed207b69909ebb05b0
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2025-01-16 16:08:29 -0300

    Add files via upload

---
+++ exercicio10/exercicio10/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio10
+{
+    internal class Pessoa
+    {
+        public double Peso { get; private set; }
+        public double Altura { get; private set; }
+
+        public Pessoa(double peso, double altura)
+        {
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public double CalcularRelacaoPesoAltura()
+        {
+            return Peso / (Altura * Altura);
+        }
+
+        public string ObterMensagemPesoIdeal()
+        {
+            double relacao = CalcularRelacaoPesoAltura();
+
+            if (relacao < 20)
+            {
+                return $"Relação: {relacao:F2}. Você está abaixo do peso.";
+            }
+            else if (relacao >= 20 && relacao < 25)
+            {
+                return $"Relação: {relacao:F2}. Você está no peso ideal.";
+            }
+            else
+            {
+                return $"Relação: {relacao:F2}. Você está acima do peso.";
+            }
+        }
+    }
+}
---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o peso da pessoa (em kg): ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da pessoa (em metros): ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(peso, altura);
+
+            Console.WriteLine($"{pessoa.ObterMensagemPesoIdeal()}");
+        }
+    }
+}
